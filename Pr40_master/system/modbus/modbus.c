8a52e65e9fe3be72310786c6c19b6ef093486b85
Dmitry Lyadnov <d.lyadnov@securitycode.ru>
2019-07-11 12:21:47 +0100

    add sources

---
+++ Pr40_master/system/modbus/modbus.c
@@ -0,0 +1,120 @@
+#include "system\modbus\modbus.h"
+#include "system\modbus\modbus_master.h"
+
+#include "system\eeprom\eeprom.h"
+#include "p33FJ128GP706.h"
+#include "system\usart\usart.h" 
+
+
+
+
+//для отладки
+unsigned int stat_modbus_all_frame=0;
+  unsigned int stat_usart_frame_bad=0;
+  unsigned int stat_usart_frame_good=0;
+    unsigned int stat_modbus_frame_error_func=0;
+    unsigned int stat_modbus_frame_error_crc=0;
+    unsigned int stat_modbus_frame_error_slave_addr=0;
+    unsigned int stat_modbus_frame_crc_ok=0;
+
+
+
+
+
+///////////////////////////////////
+void modbus_init(void)
+{
+	
+	//ножки
+#ifdef MY_MASTER_OLD
+	TRISBbits.TRISB15=0; //ножка RB15 на выходPR3
+#else
+  TRISBbits.TRISB14=0; //ножка RB14 на выход
+#endif
+	//переменные
+	  
+	//прерывания
+	
+	return;
+}
+
+///////////////////////////////////
+void modbus_eeprom_write_default_values(void)
+{
+    return;
+}
+
+
+
+
+
+
+//int data_in[256];     //данные от слейвов
+
+//extern volatile unsigned long global_clock;
+//void mypause(void)
+//{
+//
+//    unsigned long global_clock_save=global_clock;
+//
+//	while(1)
+//	{
+//		if( (global_clock-global_clock_save)>=2)
+//		{
+//			return;
+//		}
+//	}
+//}     
+
+
+void modbus_main()
+{
+//   Mbm_frame frame;
+//   int result;
+//   int data_out[256]; //данные слейвам
+// 
+//
+//   ////////////////////////////////////////////////////////////
+//   do
+//   {
+//       /* setup master packet */
+//       frame.function=3;			/* send data */
+//       frame.slave=1;					/* slave number*/ 
+//       frame.address=0;      //номер регистра который хотим прочитать
+//       frame.length=100;     //количество регистров
+//       /* setup data to send */
+//       //data_out[0]=0; //датаин юзаем только для записи в слейв
+//       result=modbus_master(frame,data_out,data_in,NULL,NULL); //датаоут - ответ от слейва
+//       //выдерживаем паузу 3.5 символа между транзакциями
+//       //UsartWaitForSilence();
+//       mypause();
+//       
+//       //пишем 0x1122 в регистр 34
+//       frame.function=6;			/* write 1 register */
+//       frame.slave=1;					/* slave number*/ 
+//       frame.address=34;      //номер регистра который хотим прочитать
+//       /* setup data to send */
+//       data_out[0]=0x1122;
+//       result=modbus_master(frame,data_out,data_in,NULL,NULL); //датаоут - ответ от слейва
+//       //выдерживаем паузу 3.5 символа между транзакциями
+//       //UsartWaitForSilence();
+//       mypause();
+//
+//        //функция 0x44 - функция начать измерения по всем датчикам 
+//       frame.function=0x44;			/* write 1 register */
+//       frame.slave=MODBUS_BROADCAST_ADDR;
+//       result=modbus_master(frame,data_out,data_in,NULL,NULL); //датаоут - ответ от слейва
+//       //выдерживаем паузу 3.5 символа между транзакциями
+//       //UsartWaitForSilence();
+//       mypause();
+//        
+// 
+// 
+//   }while(1);    
+//   
+   
+   
+   
+   return;
+}
+ 
---
+++ Pr40_slave/system/modbus/modbus.c
@@ -0,0 +1,225 @@
+#include "system\modbus\modbus.h"
+#include "system\modbus\modbus_slave.h"
+
+#include "system\eeprom\eeprom.h"
+#include "system\adc\adc.h"
+#include "p33FJ128GP706.h"
+
+
+unsigned int slave_regs[TOTAL_REG_NUM];
+
+unsigned char  cfg_slave_addr;
+unsigned short cfg_start_pause;
+unsigned short cfg_measure_interval;
+unsigned short cfg_k[NUMBER_OF_SENSORS];
+
+///////////////////////////////////
+void modbus_init(void)
+{
+  int i;
+  
+  //ножки
+
+  //переменные EEPROM
+  cfg_slave_addr=eeprom_read(ADDR_EEPROM_SLAVE_ADDR);
+  eeprom_read_buf(ADDR_EEPROM_START_PAUSE,(unsigned char*)&cfg_start_pause,2);
+  eeprom_read_buf(ADDR_EEPROM_MEASURE_INTERVAL,(unsigned char*)&cfg_measure_interval,2);
+  for(i=0;i<NUMBER_OF_SENSORS;i++)
+  {
+    eeprom_read_buf(ADDR_EEPROM_K1+i*2,(unsigned char*)&cfg_k[i],2);
+  }
+  
+  //регистры MODBUS
+  for(i=0;i<TOTAL_REG_NUM;i++) slave_regs[i]=0;
+  slave_regs[REG_MAGIC]=0x1234;
+  slave_regs[REG_CONFIG_SLAVE_ADDR]=cfg_slave_addr;
+  slave_regs[REG_CONFIG_START_PAUSE]=cfg_start_pause;
+  slave_regs[REG_CONFIG_MEASURE_INTERVAL]=cfg_measure_interval;
+  for(i=0;i<NUMBER_OF_SENSORS;i++)
+  {
+    slave_regs[REG_CONFIG_K1+i]=cfg_k[i];
+  }
+    
+  //прерывания
+  
+  return;
+}
+
+///////////////////////////////////
+void modbus_eeprom_write_default_values(void)
+{
+  int i;
+
+  eeprom_write_byte(ADDR_EEPROM_SLAVE_ADDR,DEFAULT_SLAVE_ADDR);
+  eeprom_write_word(ADDR_EEPROM_START_PAUSE,DEFAULT_START_PAUSE);
+  eeprom_write_word(ADDR_EEPROM_MEASURE_INTERVAL,DEFAULT_MEASURE_INTERVAL);
+  for(i=0;i<NUMBER_OF_SENSORS-1;i++)
+  {
+    //значение по-умолчанию для датчиков давления
+    eeprom_write_word(ADDR_EEPROM_K1+i*2,DEFAULT_K);
+  }
+  //значение по-умолчанию для дачтика сопротивления воды
+  eeprom_write_word(ADDR_EEPROM_K1+(NUMBER_OF_SENSORS-1)*2,DEFAULT_R);
+  
+  return;
+}
+
+
+
+void modbus_main(void)
+{
+    modbus_slave_loop(); //в этом цикле висим вечно
+
+   return;
+}
+
+
+void modbus_read_reg_func(unsigned int addr, unsigned int num)
+{
+  int i;
+  unsigned long temp;
+  
+  if(addr>=REG_SENSOR1_RECORD_VALUE) return; //на лету вычисляем только первые 0x64 регистра
+  
+  for(i=addr;i<(addr+num);i++)
+  {
+    if( (i>=REG_SENSOR1_CURRENT_VALUE) && (i<=REG_SENSOR10_CURRENT_VALUE) )
+    {   
+      //slave_regs[i]=ADC_get(i-REG_SENSOR1_CURRENT_VALUE+1);
+      
+      //алгоритм калибровочного коэффициента:
+      //есть 
+      //АЦП           0..4095
+      //Давление, мАтм  0..16000 = это и есть К, т.е K=это максимальное давление в мАтм, соответсвующее максимальному АЦП=4095
+      //текущее значение мАтм = текАЦП*К(2байта)/4095
+      //т.е на мастер передаем значение в мАтм, а мастер делит на 1000 и выводит на экран в Атм
+
+      temp=ADC_get(i-REG_SENSOR1_CURRENT_VALUE);
+      //temp=(temp*slave_regs[REG_CONFIG_K1+j])>>12;
+      temp=(temp*slave_regs[REG_CONFIG_K1+i-REG_SENSOR1_CURRENT_VALUE])/4095;
+      slave_regs[i]=temp;
+    }
+    if(i==REG_WATER_LEVEL_CURRENT_VALUE)
+    {
+      slave_regs[i]=PORTGbits.RG6; //датчик уровня воды
+    }    
+  }
+  return;
+}
+
+
+void modbus_write_reg_func(unsigned int addr,unsigned int value)
+{
+  int i;
+  switch (addr)
+  {
+    case REG_CONFIG_SLAVE_ADDR:
+    case REG_CONFIG_START_PAUSE:
+    case REG_CONFIG_MEASURE_INTERVAL:
+    case REG_CONFIG_K1:
+    case REG_CONFIG_K2:
+    case REG_CONFIG_K3:  
+    case REG_CONFIG_K4:  
+    case REG_CONFIG_K5:  
+    case REG_CONFIG_K6:  
+    case REG_CONFIG_K7:  
+    case REG_CONFIG_K8:  
+    case REG_CONFIG_K9:  
+    case REG_CONFIG_K10:  
+      slave_regs[addr]=value; //dml!!! проверку на допустимый диапазон добавить тут?
+      break;
+    case REG_CONTROL_MEASURE_START:
+      if(value==CMD_MEASURE_START)
+      {
+        modbus_start_full_measurement();
+        slave_regs[REG_STAT_FULL_DUMP_NUM]++;
+      }
+      break;
+    case REG_CONTROL_SAVE:
+      if(value==CMD_SAVE_K)
+      {
+          for(i=0;i<NUMBER_OF_SENSORS;i++)
+          {
+            if(cfg_k[i]!=slave_regs[REG_CONFIG_K1+i])
+            {
+              cfg_k[i]=slave_regs[REG_CONFIG_K1+i];
+              eeprom_write_word(ADDR_EEPROM_K1+i*2,cfg_k[i]);
+            }
+          }
+      }
+      else if(value==CMD_SAVE_SLAVE_ADDR)
+      {
+        cfg_slave_addr=slave_regs[REG_CONFIG_SLAVE_ADDR];
+        eeprom_write_byte(ADDR_EEPROM_SLAVE_ADDR,cfg_slave_addr);
+      }
+      else if(value==CMD_SAVE_START_PAUSE)
+      {
+        cfg_start_pause=slave_regs[REG_CONFIG_START_PAUSE];
+        eeprom_write_word(ADDR_EEPROM_START_PAUSE,cfg_start_pause);
+      }
+      else if(value==CMD_SAVE_MEASURE_INTERVAL)
+      {
+        cfg_measure_interval=slave_regs[REG_CONFIG_MEASURE_INTERVAL];
+        eeprom_write_byte(ADDR_EEPROM_MEASURE_INTERVAL,cfg_measure_interval);
+      }
+      break;
+    default:
+      break; 
+  
+  }
+   return;
+}
+
+
+
+void modbus_start_full_measurement(void)
+{
+    int i,j;
+    unsigned long temp;
+   
+    //выдерживаем начальную паузу перед стартом измерений
+    T8CON=0;
+    T8CONbits.T32=1;        //связка  TMR9:TMR8 - работает в 32х-битном режиме
+	T8CONbits.TCS=0;        //Timer8 Clock Source Select bit: Internal clock (Fcy=40MHz=Fosc/2=80мгц/2
+    T8CONbits.TCKPS=3;      //1:256  1такт=25*256нс=6.4мкс
+    PR8=( (unsigned long)cfg_start_pause*156 ) & 0xFFFF;  //1мс=256(предделитель)*25нс*156  младшее слово периода 32х-битного таймера
+	PR9=( (unsigned long)cfg_start_pause*156 ) >> 16;      //1мс=256(предделитель)*25нс*156  старшее слово
+    TMR8=0;
+	TMR9=0;
+    T8CONbits.TON=1; //включаем таймер 8-9
+    while(IFS3bits.T9IF==0);//T9 а не T8!
+    IFS3bits.T9IF=0;  //сбрасываем флаг
+    
+    
+    //подготавливаем таймер к паузам между измерениями   
+    T8CON=0;
+    T8CONbits.TCS=0;        //Timer8 Clock Source Select bit: Internal clock (Fcy=40MHz=Fosc/2=80мгц/2
+    T8CONbits.TCKPS=3;      //1:256  1такт=25*256нс=6.4мкс
+    PR8=cfg_measure_interval*156;  //1мс=256(предделитель)*25нс*156
+    TMR8=0;
+    T8CONbits.TON=1; //включаем таймер 8
+    
+    //запускаем измерение давления по всем датчикам
+    for(i=0;i<NUMBER_OF_MEASUREMENTS;i++)
+    {
+        for(j=0;j<(NUMBER_OF_SENSORS-1);j++) //вот этот цикл целиком выполняется 448 мкс где-то
+        {
+          //алгоритм калибровочного коэффициента:
+          //есть 
+          //АЦП           0..4095
+          //Давление, мАтм  0..16000 = это и есть К
+          //текущее значение мАтм = текАЦП*К(2байта)/4095
+          //т.е на мастер передаем значение в мАтм, а мастер делит на 1000 и выводит на экран в Атм
+
+          temp=ADC_get(j);
+          //temp=(temp*slave_regs[REG_CONFIG_K1+j])>>12;
+          temp=(temp*slave_regs[REG_CONFIG_K1+j])/4095;
+          slave_regs[REG_SENSOR1_RECORD_VALUE+(j*NUMBER_OF_MEASUREMENTS)+i]=temp;
+        }
+        //выдерживаем паузу между измерениями
+        while(IFS3bits.T8IF==0);
+        IFS3bits.T8IF=0;  //сбрасываем флаг
+    }
+   
+    return;
+}
