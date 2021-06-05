75aac24d7f338ce14e7308b9e71e42a3f1b57b42
dml <lyadnov@gmail.com>
2021-06-05 09:57:15 +0300

    modbus: additional verifications/refactoring

--- Pr40_slave/system/modbus/modbus.c
+++ Pr40_slave/system/modbus/modbus.c
@@ -107,66 +107,84 @@ void modbus_read_reg_func(unsigned int addr, unsigned int num)
 }
 
 
+static void modbus_save_reg(unsigned int value)
+{
+	int i;
+	switch (value)
+	{
+		case CMD_SAVE_K:
+			for(i = 0; i < NUMBER_OF_SENSORS; i++)
+			{
+				if (cfg_k[i] != slave_regs[REG_CONFIG_K1 + i])
+				{
+					cfg_k[i] = slave_regs[REG_CONFIG_K1 + i];
+					eeprom_write_word(ADDR_EEPROM_K1 + i * 2, cfg_k[i]);
+				}
+			}
+			break;
+		case CMD_SAVE_SLAVE_ADDR:
+			cfg_slave_addr=slave_regs[REG_CONFIG_SLAVE_ADDR];
+			eeprom_write_byte(ADDR_EEPROM_SLAVE_ADDR,cfg_slave_addr);
+			break;
+		case CMD_SAVE_START_PAUSE:
+			cfg_start_pause=slave_regs[REG_CONFIG_START_PAUSE];
+			eeprom_write_word(ADDR_EEPROM_START_PAUSE,cfg_start_pause);
+			break;
+		case CMD_SAVE_MEASURE_INTERVAL:
+			cfg_measure_interval=slave_regs[REG_CONFIG_MEASURE_INTERVAL];
+			eeprom_write_byte(ADDR_EEPROM_MEASURE_INTERVAL,cfg_measure_interval);
+			break;
+		default:
+			break;
+	}
+}
+
+
 void modbus_write_reg_func(unsigned int addr,unsigned int value)
 {
-  int i;
-  switch (addr)
-  {
-    case REG_CONFIG_SLAVE_ADDR:
-    case REG_CONFIG_START_PAUSE:
-    case REG_CONFIG_MEASURE_INTERVAL:
-    case REG_CONFIG_K1:
-    case REG_CONFIG_K2:
-    case REG_CONFIG_K3:  
-    case REG_CONFIG_K4:  
-    case REG_CONFIG_K5:  
-    case REG_CONFIG_K6:  
-    case REG_CONFIG_K7:  
-    case REG_CONFIG_K8:  
-    case REG_CONFIG_K9:  
-    case REG_CONFIG_K10:  
-      slave_regs[addr]=value; //dml!!! проверку на допустимый диапазон добавить тут?
-      break;
-    case REG_CONTROL_MEASURE_START:
-      if(value==CMD_MEASURE_START)
-      {
-        modbus_start_full_measurement();
-        slave_regs[REG_STAT_FULL_DUMP_NUM]++;
-      }
-      break;
-    case REG_CONTROL_SAVE:
-      if(value==CMD_SAVE_K)
-      {
-          for(i=0;i<NUMBER_OF_SENSORS;i++)
-          {
-            if(cfg_k[i]!=slave_regs[REG_CONFIG_K1+i])
-            {
-              cfg_k[i]=slave_regs[REG_CONFIG_K1+i];
-              eeprom_write_word(ADDR_EEPROM_K1+i*2,cfg_k[i]);
-            }
-          }
-      }
-      else if(value==CMD_SAVE_SLAVE_ADDR)
-      {
-        cfg_slave_addr=slave_regs[REG_CONFIG_SLAVE_ADDR];
-        eeprom_write_byte(ADDR_EEPROM_SLAVE_ADDR,cfg_slave_addr);
-      }
-      else if(value==CMD_SAVE_START_PAUSE)
-      {
-        cfg_start_pause=slave_regs[REG_CONFIG_START_PAUSE];
-        eeprom_write_word(ADDR_EEPROM_START_PAUSE,cfg_start_pause);
-      }
-      else if(value==CMD_SAVE_MEASURE_INTERVAL)
-      {
-        cfg_measure_interval=slave_regs[REG_CONFIG_MEASURE_INTERVAL];
-        eeprom_write_byte(ADDR_EEPROM_MEASURE_INTERVAL,cfg_measure_interval);
-      }
-      break;
-    default:
-      break; 
-  
-  }
-   return;
+	switch (addr)
+	{
+		case REG_CONFIG_SLAVE_ADDR:
+			if ((value < 1) || (value > 247)) //см. https://ru.wikipedia.org/wiki/Modbus#%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5
+				return;
+			break;
+		case REG_CONFIG_START_PAUSE:
+			if ((value < START_PAUSE_MIN_VALUE) || (value > START_PAUSE_MAX_VALUE))
+				return;
+			break;
+		case REG_CONFIG_MEASURE_INTERVAL:
+			if ((value < 1) || (value > 100))
+				return;
+			break;
+		case REG_CONFIG_K1:
+		case REG_CONFIG_K2:
+		case REG_CONFIG_K3:  
+		case REG_CONFIG_K4:  
+		case REG_CONFIG_K5:  
+		case REG_CONFIG_K6:  
+		case REG_CONFIG_K7:  
+		case REG_CONFIG_K8:  
+		case REG_CONFIG_K9:  
+		case REG_CONFIG_K10:  
+			if ((value < 1) || (value > K_MAX_VALUE))
+				return;
+			break;
+		case REG_CONTROL_MEASURE_START:
+			if(value == CMD_MEASURE_START)
+			{
+				modbus_start_full_measurement();
+				slave_regs[REG_STAT_FULL_DUMP_NUM]++;
+			}
+			return;
+		case REG_CONTROL_SAVE:
+			modbus_save_reg(value);
+			return;
+		default:
+			return;   //иначе, если addr > TOTAL_REG_NUM, то запортим ОЗУ
+	}
+	
+	slave_regs[addr]=value;
+	return;
 }
 
 
